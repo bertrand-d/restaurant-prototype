f13b57fa8891b203d0f940707c6ff6b0427562c0
reuby-dev <delphineb57@gmail.com>
2023-03-22 17:39:28 +0100

    add hostel

--- frontend/src/components/cardDetailedTop.js
+++ frontend/src/components/cardDetailedTop.js
@@ -1,17 +1,21 @@
 import Icon from "./Icon"
-import image from "../images/hebergement/annie-spratt.jpg"
 
 export default function CardDetailedTop(props) {
+
+    const {name, price, picture} = props.hostel.attributes
+
+    const url = "http://localhost:1337" + picture.data.attributes.url
 
     return (
         <a className="card-detailed-top" href="#">
             <div className="card-detailed-top__top-container">
-                <img src={image} alt="hotel" />
+                <img src={url} alt="hotel" />
             </div>
             <div className="card-detailed-top__bottom-container">
-                <span className="card-detailed-top__title">Titre de l'hotel</span>
+                <span className="card-detailed-top__title">{name}</span>
                 <span className="card-detailed-top__description">
-                    Nuit à partir de <span className="card-detailed-top__description__currency">€</span>
+                    Nuit à partir de {price}
+                    <span className="card-detailed-top__description__currency"> €</span>
                 </span>
                 <div className="card-detailed-top__notation">
                     <Icon name="dog" />
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,12 +1,51 @@
 import SearchBlock from "../components/SearchBlock"
-import CardDetailedTop from "../components/cardDetailedTop"
+import CardDetailedTop from "../components/CardDetailedTop"
+import React, { useState, useEffect } from 'react'
 
 export default function Homepage() {
+
+
+  const [hostels, setHostels] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:1337/api/hotels?populate=*")
+      .then(res => res.json())
+      .then(
+        (result) => {
+           setHostels(result.data)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+
+        }
+      )
+  }, 
+    // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  [])
+
+  console.log(hostels)
 
     return (
         <>
             <SearchBlock/>
-            <CardDetailedTop/>
+            <section className="accomodation-result">
+              <div className="accomodation">
+                <h2>Hébergements à Marseille</h2>
+                <div className="accomodation__container"></div>
+                {
+                  React.Children.toArray(hostels.map((hostel) => <CardDetailedTop hostel= {hostel}/> ))
+                }
+
+              </div>
+              <div className="popular-accomodation">
+                <h2>Les plus populaires</h2>
+
+              </div>
+            </section>
         </>
     );
 }
